eb2ae685bef181fe1cea4800527ce572517d09ad
Fran Boon <fran@aidiq.com>
2012-04-15 19:21:04 +0100

    Tweak Popups/Cluster settings

--- modules/s3/s3gis.py
+++ modules/s3/s3gis.py
@@ -2089,7 +2089,7 @@ class GIS(object):
                     if value:
                         field = table[fieldname]
                         represent = self.get_representation(field, value)
-                        tooltip = "%s<br />%s" % (tooltip, represent)
+                        tooltip = "%s<br/>%s" % (tooltip, represent)
             except:
                 # This field isn't in the table
                 pass
@@ -3370,7 +3370,7 @@ class GIS(object):
         # Defaults
         # Also in static/S3/s3.gis.js
         # http://dev.openlayers.org/docs/files/OpenLayers/Strategy/Cluster-js.html
-        self.cluster_distance = 2    # pixels
+        self.cluster_distance = 20   # pixels
         self.cluster_threshold = 2   # minimum # of features to form a cluster
 
         # Support bookmarks (such as from the control)
